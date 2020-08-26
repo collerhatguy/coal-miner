24cf8a2c93a8fe33ea719da3d8187cc736e2cfcb
collerhatguy <jacobshomali3@gmail.com>
2020-08-26 09:18:44 -0400

    Updates

---
+++ precache-manifest.0ad23abed46bd9fa64a5b2ba3f867831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b31e496b673f3f3f5bfbddd9e09704a",
+    "url": "/coal-miner/index.html"
+  },
+  {
+    "revision": "b3d7e4fe0d263012b9fe",
+    "url": "/coal-miner/static/css/main.a8179753.chunk.css"
+  },
+  {
+    "revision": "b4a6d1b644bf7c3fc631",
+    "url": "/coal-miner/static/js/2.1e2bd7ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coal-miner/static/js/2.1e2bd7ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3d7e4fe0d263012b9fe",
+    "url": "/coal-miner/static/js/main.e1600392.chunk.js"
+  },
+  {
+    "revision": "5ec4e235b1eb62f7bb2a",
+    "url": "/coal-miner/static/js/runtime-main.466c42b5.js"
+  }
+]);
